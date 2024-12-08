720409d43e287cc53c5757a1df45def7b493d348
LordDomon <lorddomon001@gmail.com>
2024-12-08 13:07:55 +0100

    Yalla Salamalkumum Habibi

--- logic.py
+++ logic.py
@@ -1,67 +1,125 @@
-from random import randint
+from random import randint, choice
 import requests
+
 
 class Pokemon:
     pokemons = {}
 
     def __init__(self, pokemon_trainer):
         self.pokemon_trainer = pokemon_trainer
-        self.pokemon_number = randint(1, 1000)
+        self.pokemon_number = randint(900, 1000) 
         self.abilities = self.get_abilities()
         self.img = self.get_img()
         self.name = self.get_name()
         self.rarity = self.get_rarity()
+        self.health = self.get_health()
+        self.power = self.get_power()
+        self.is_weak = False  
 
         Pokemon.pokemons[pokemon_trainer] = self
+
+    def attack(self, enemy):
+        if enemy.health > self.power:
+            enemy.health -= self.power
+            return f"Сражение @{self.pokemon_trainer} с @{enemy.pokemon_trainer}"
+        else:
+            enemy.health = 0
+            return f"Победа @{self.pokemon_trainer} над @{enemy.pokemon_trainer}!"
 
     def get_abilities(self):
         url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
         try:
             response = requests.get(url)
             response.raise_for_status()
-            data = response.json()
-            return data['abilities'][0]['ability']['name']
-        except requests.exceptions.RequestException as e:
-            return f"Error: {e}"
+            if response.status_code == 200:
+                data = response.json()
+                return data['abilities'][0]['ability']['name']
+        except requests.exceptions.RequestException:
+            return "Неизвестная способность"
 
     def get_img(self):
         url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
         try:
             response = requests.get(url)
             response.raise_for_status()
-            data = response.json()
-            return data['sprites']['front_shiny']
-        except requests.exceptions.RequestException as e:
-            return "th.jpg"
+            if response.status_code == 200:
+                data = response.json()
+                return data['sprites']['front_shiny']
+        except requests.exceptions.RequestException:
+            return "th.jpg" 
 
     def get_name(self):
         url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
         try:
             response = requests.get(url)
             response.raise_for_status()
-            data = response.json()
-            return data['forms'][0]['name']
-        except requests.exceptions.RequestException as e:
-            return "Pikachu"
+            if response.status_code == 200:
+                data = response.json()
+                return data['forms'][0]['name']
+        except requests.exceptions.RequestException:
+            return f"Pokemon#{self.pokemon_number}"
 
     def get_rarity(self):
-        if self.pokemon_number <= 100:
+        # Устанавливаем редкость в зависимости от случайного номера
+        if self.pokemon_number <= 300:
             return "Редкий"
-        elif self.pokemon_number <= 500:
+        elif self.pokemon_number <= 720:
             return "Сверхредкий"
         elif self.pokemon_number <= 900:
             return "Эпический"
         else:
-            return "ЛЕГА ААААА"
+            return "Легендарный"
+
+    def get_health(self):
+        # Здоровье покемона зависит от его редкости
+        if self.rarity == "Легендарный":
+            return randint(150, 250)
+        elif self.rarity == "Эпический":
+            return randint(120, 200)
+        else:
+            return randint(80, 150)
+
+    def get_power(self):
+        # Сила покемона зависит от его редкости
+        if self.rarity == "Легендарный":
+            return randint(100, 150)
+        elif self.rarity == "Эпический":
+            return randint(80, 120)
+        elif self.rarity == "Сверхредкий":
+            return randint(60, 100)
+        else:
+            return randint(50, 80)
 
     def info(self):
-        return f"Имя твоего покемона: {self.name}, способность: {self.abilities}, редкость: {self.rarity}"
+        return (f"Имя: {self.name}, Способность: {self.abilities}, "
+                f"Редкость: {self.rarity}, Здоровье: {self.health}, Атака: {self.power}")
 
     def show_img(self):
-            return self.img
+        return self.img
 
-    def get_music(self):
-        if self.rarity == "ЛЕГА ААААА":
-            return "https://www.youtube.com/watch?v=UbQKpQ7n2Mw"
-        else:
-            return None
+    def check_if_weak(self):
+        if self.health <= 50:
+            self.is_weak = True
+            return True
+        return False
+
+
+class Fighter(Pokemon):
+    def get_health(self):
+        return randint(100, 200)   
+
+    def get_power(self):
+        return randint(100, 200)
+
+
+class Wizard(Pokemon):
+    def get_health(self):
+        return randint(80, 150) 
+
+    def get_power(self):
+        return randint(200, 300)
+
+
+class Defender(Pokemon):
+    def get_health(self):
+        return randint(300, 500)  
--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 import telebot
 from config import token
-from logic import Pokemon
+from logic import Pokemon, Wizard, Fighter
+from random import randint, choice
 
 bot = telebot.TeleBot(token) 
-
 @bot.message_handler(commands=['go'])
 def go(message):
     if message.from_user.username not in Pokemon.pokemons.keys():
         pokemon = Pokemon(message.from_user.username)
 
 
+        chance = randint(1,3)
+        if chance == 1:
+            pokemon = Pokemon(message.from_user.username)
+        elif chance == 2:
+            pokemon = Wizard(message.from_user.username)
+        elif chance == 3:
+            pokemon = Fighter(message.from_user.username)
         bot.send_message(message.chat.id, pokemon.info())
         bot.send_photo(message.chat.id, pokemon.show_img())
      
-        if pokemon.rarity == "ЛЕГА ААААА":
+        if pokemon.rarity == "Легендарный":
             bot.send_message(message.chat.id, "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA LEOOOOON!!!!!!!! LEON!!!!!!!! AAAAAAAAAAAAAAAAA")
             bot.send_photo(message.chat.id, pokemon.show_img()) 
             bot.send_photo(message.chat.id, open('OIP.jpg', 'rb'))
@@ -21,27 +28,135 @@ def go(message):
             bot.send_photo(message.chat.id, pokemon.show_img()) 
             bot.send_photo(message.chat.id, open('456841065-min-1200x834.jpg', 'rb'))
             bot.send_photo(message.chat.id, pokemon.show_img()) 
+            bot.send_message(message.chat.id, "https://www.youtube.com/watch?v=WPZ6BR9w-qg&t=10s")
             bot.send_photo(message.chat.id, open('john-cena-1553-x-1336-picture-ve1c97sbz4cg9wpw.jpg', 'rb'))
             bot.send_message(message.chat.id, " JOOOOOHN CEEEENAAAAAA") 
             bot.send_photo(message.chat.id, open('th.jpg', 'rb'))
             bot.send_photo(message.chat.id, pokemon.show_img()) 
-            bot.send_message(message.chat.id, "https://www.youtube.com/watch?v=WPZ6BR9w-qg&t=10s")
             bot.send_message(message.chat.id, "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA LEOOOOON!!!!!!!! LEON!!!!!!!! AAAAAAAAAAAAAAAAA")  
-            bot.send_photo(message.chat.id, pokemon.show_img())
-            bot.send_photo(message.chat.id, open('R.jpg', 'rb'))
+            bot.send_message(message.chat.id, "https://www.youtube.com/watch?v=aVB1EY3w3lc&t=52s")  
             bot.send_message(message.chat.id, "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA LEOOOOON!!!!!!!! LEON!!!!!!!! AAAAAAAAAAAAAAAAA")
-            bot.send_photo(message.chat.id, pokemon.show_img())
-            bot.send_photo(message.chat.id, open('456841065-min-1200x834.jpg', 'rb'))
+            bot.send_photo(message.chat.id, pokemon.show_img()) 
             bot.send_photo(message.chat.id, open('OIP.jpg', 'rb'))
             bot.send_message(message.chat.id, " JOOOOOHN CEEEENAAAAAA")
+            bot.send_photo(message.chat.id, pokemon.show_img()) 
+            bot.send_photo(message.chat.id, open('456841065-min-1200x834.jpg', 'rb'))
+            bot.send_photo(message.chat.id, pokemon.show_img()) 
+            bot.send_message(message.chat.id, "https://www.youtube.com/watch?v=WPZ6BR9w-qg&t=10s")
             bot.send_photo(message.chat.id, open('john-cena-1553-x-1336-picture-ve1c97sbz4cg9wpw.jpg', 'rb'))
+            bot.send_message(message.chat.id, " JOOOOOHN CEEEENAAAAAA") 
             bot.send_photo(message.chat.id, open('th.jpg', 'rb'))
-            bot.send_photo(message.chat.id, pokemon.show_img())
-            bot.send_message(message.chat.id, "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+            bot.send_photo(message.chat.id, pokemon.show_img()) 
+            bot.send_message(message.chat.id, "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA LEOOOOON!!!!!!!! LEON!!!!!!!! AAAAAAAAAAAAAAAAA")  
             bot.send_message(message.chat.id, "https://www.youtube.com/watch?v=aVB1EY3w3lc&t=52s")  
-            
 
     else:
         bot.reply_to(message, "Ты уже создал себе покемона")
 
+@bot.message_handler(commands=['fight'])
+def start_fight(message):
+    if message.from_user.username in Pokemon.pokemons:
+        player_pokemon = Pokemon.pokemons[message.from_user.username]
+
+        
+        enemy_classes = [Pokemon, Wizard, Fighter]
+        enemy_class = choice(enemy_classes)
+        enemy_pokemon = enemy_class("Случайный противник")
+
+        bot.send_message(message.chat.id, f"Ваш покемон:\n{player_pokemon.info()}")
+        bot.send_photo(message.chat.id, player_pokemon.show_img())
+        bot.send_message(message.chat.id, f"Противник:\n{enemy_pokemon.info()}")
+        bot.send_photo(message.chat.id, enemy_pokemon.show_img())
+
+
+        round_counter = 1
+        while player_pokemon.health > 0 and enemy_pokemon.health > 0:
+            bot.send_message(message.chat.id, f"Раунд {round_counter}!")
+
+
+            bot.send_message(message.chat.id, f"{player_pokemon.name} атакует {enemy_pokemon.name}!")
+            enemy_pokemon.health -= player_pokemon.power
+            if enemy_pokemon.health <= 0:
+                bot.send_message(message.chat.id, f"Ваш {player_pokemon.name} победил {enemy_pokemon.name}!")
+                return
+
+          
+            bot.send_message(message.chat.id, f"{enemy_pokemon.name} атакует {player_pokemon.name}!")
+            player_pokemon.health -= enemy_pokemon.power
+            if player_pokemon.health <= 0:
+                bot.send_message(message.chat.id, f"{enemy_pokemon.name} победил вашего {player_pokemon.name}!")
+                return
+
+            round_counter += 1
+            bot.send_message(message.chat.id, f"{player_pokemon.name}: {player_pokemon.health} здоровья")
+            bot.send_message(message.chat.id, f"{enemy_pokemon.name}: {enemy_pokemon.health} здоровья")
+
+@bot.message_handler(commands=["train"])
+def train(message):
+    if message.from_user.username in Pokemon.pokemons:
+        pokemon = Pokemon.pokemons[message.from_user.username]
+        
+       
+        increase_power = randint(10, 25)
+        increase_health = randint(1, 6)
+        pokemon.power += increase_power
+        pokemon.health -= increase_health
+
+        bot.send_message(
+            message.chat.id,
+            f"Ваш покемон {pokemon.name} потренировался! 💪\n"
+           
+            f"Текущие характеристики:\n"
+            f"Сила: {pokemon.power}, Здоровье: {pokemon.health}"
+        )
+    else:
+        bot.send_message(
+            message.chat.id,
+            "У вас пока нет покемона. Введите /go, чтобы создать его!"
+        )
+
+@bot.message_handler(commands=["feed"])
+def feed(message):
+    if message.from_user.username in Pokemon.pokemons:
+        pokemon = Pokemon.pokemons[message.from_user.username]
+        
+
+        restored_health = randint(20, 50)
+        pokemon.health += restored_health
+
+        bot.send_message(
+            message.chat.id,
+            f"Ваш покемон {pokemon.name} поел! 🍎\n"
+            f"⬆️ Здоровье восстановилось на {restored_health}!\n"
+            f"Текущее здоровье: {pokemon.health}"
+        )
+    else:
+        bot.send_message(
+            message.chat.id,
+            "У вас пока нет покемона. Введите /go, чтобы создать его!"
+        )
+
+@bot.message_handler(commands=["show"])
+def show_pokemon(message):
+    if message.from_user.username in Pokemon.pokemons:
+        pokemon = Pokemon.pokemons[message.from_user.username]
+        
+
+        bot.send_message(
+            message.chat.id,
+            f"👾 Ваш покемон:\n"
+            f"Имя: {pokemon.name}\n"
+            f"Способность: {pokemon.abilities}\n"
+            f"Редкость: {pokemon.rarity}\n"
+            f"Здоровье: {pokemon.health}\n"
+            f"Атака: {pokemon.power}"
+        )
+        
+        
+        bot.send_photo(message.chat.id, pokemon.show_img())
+    else:
+        bot.send_message(
+            message.chat.id,
+            "У вас пока нет покемона. Введите /go, чтобы создать его!"
+        )
 bot.infinity_polling(none_stop=True)
